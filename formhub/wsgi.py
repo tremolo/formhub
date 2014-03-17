39ef5b2d6f1ffdfb34d8519c675f55b71deb0269
vernondcole <vernon.cole@ehealthnigeria.org>
2014-03-17 18:15:04 +0100

    uWSGI settings for staging

--- formhub/wsgi.py
+++ formhub/wsgi.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "formhub.preset.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "formhub.preset.staging")
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.

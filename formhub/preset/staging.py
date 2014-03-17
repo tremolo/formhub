c20c5cca9e6ad0fe3943f8d49207eaf85dbe5c25
vernondcole <vernon.cole@ehealthnigeria.org>
2014-03-17 12:37:13 +0100

    make staging use RabbitMQ

--- formhub/preset/staging.py
+++ formhub/preset/staging.py
@@ -31,10 +31,7 @@ if TESTING_MODE:
     MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'test_media/')
     subprocess.call(["rm", "-r", MEDIA_ROOT])
     MONGO_DATABASE['NAME'] = "formhub_test"
-    # need to have CELERY_ALWAYS_EAGER True and BROKER_BACKEND as memory
-    # to run tasks immediately while testing
-    CELERY_ALWAYS_EAGER = True
-    BROKER_BACKEND = 'memory'
+
     ENKETO_API_TOKEN = 'abc'
     #TEST_RUNNER = 'djcelery.contrib.test_runner.CeleryTestSuiteRunner'
 else:

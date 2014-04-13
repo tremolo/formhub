8b7d63224225017444975cf3ad17026eb20bbf54
vernondcole <vernon.cole@ehealthnigeria.org>
2014-04-13 22:00:16 +0100

    corrections to config for production

--- formhub/preset/production.py
+++ formhub/preset/production.py
@@ -1,37 +1,38 @@
 # this system uses structured settings.py as defined in http://www.slideshare.net/jacobian/the-best-and-worst-of-django
 
-try:
-    from ..settings import *
-except ImportError:
-    import sys, django
-    django.utils.six.reraise(RuntimeError, *sys.exc_info()[1:])  # use RuntimeError to extend the traceback
-except:
-    raise
+from formhub.settings import *
 
 DEBUG = False  # this setting file will not work on "runserver" -- it needs a server for static files
+
+ADMINS = (
+    ('Adam', 'adam@ehealthafrica.org'),
+)
+
+MANAGERS = ADMINS
 
 # override to set the actual location for the production static and media directories
 MEDIA_ROOT = '/var/formhub-media'
 STATIC_ROOT = "/srv/formhub-static"
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, "static"),
-)
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
+
 # your actual production settings go here...,.
-# for 12-factor installations, this will be in a URL read by default_settings.py
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'formhub',
-#         'USER': 'formhub_prod',
-#         'PASSWORD': os.environ['FORMHUB_PROD_PW'],  # the password must be stored in an environment variable
-#         'HOST': os.environ.get("FORMHUB_DB_SERVER", 'dbserver.yourdomain.org'), # the server name may be in env
-#         'OPTIONS': {
-#             'autocommit': True,   # note: this option obsolete starting with django 1.6
-#         }
-#     },
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'phis',
+        'USER': 'phis',
+        'PASSWORD': 'nopolio4u', # os.environ['PHIS_PW'],  # the password must be stored in an environment variable
+        'HOST': 'nomads.eocng.org',
+        'OPTIONS': {
+            'autocommit': True,
+        }
+    }
+}
+
+#
+ALLOWED_HOSTS = ['formhub.eocng.org', 'forms.ehealthafrica.org',
+		'forms.ehealth.org.ng']
+
+#DATABASE_ROUTERS = ['formhub.preset.dbrouter.GisRouter']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -40,8 +41,19 @@ ADMINS = (
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/New_York'
-#TIME_ZONE = 'Africa/Lagos'
+TIME_ZONE = 'Africa/Lagos'
+
+BROKER_URL = 'amqp://formhub:12345678@localhost:5672/formhub_vhost'
+
+EMAIL_HOST = 'smtp.gmail.com'  #The host to use for sending email.
+
+EMAIL_HOST_PASSWORD = "donotreply12345"  #os.environ.get("FORMHUB_EMAIL_PASSWORD", "12345678")
+#Password to use for the SMTP server defined in EMAIL_HOST.
+EMAIL_HOST_USER = 'do.not.reply@ehealthnigeria.org'
+
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "do.not.reply@ehealthafrica.org"
 
 TOUCHFORMS_URL = 'http://localhost:9000/'
 
@@ -53,4 +65,9 @@ MONGO_DATABASE = {
     'PASSWORD': ''
 }
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'mlfs33^s1l4xf6a36$0#j%dd*sisfo6HOk3245u3tYXB9y'
+SECRET_KEY = 'mlfs33^s1l4xf6a36$0#j%dd*sisfo6HOktYXB9y'
+
+TESTING_MODE = False
+
+ENKETO_URL = 'https://enketo.org/'
+ENKETO_API_TOKEN = 'mo4ao9nrr5a2x1or'
